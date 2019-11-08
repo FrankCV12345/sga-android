d662827232a2648b01dbbd344138aef753dbe162
Your Name <you@example.com>
2019-11-08 08:25:06 -0500

    agrego el toolbar

--- app/src/main/java/com/frank/sga/MenuPrincipal.java
+++ app/src/main/java/com/frank/sga/MenuPrincipal.java
@@ -1,14 +1,38 @@
 package com.frank.sga;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+//import androidx.appcompat.widget.Toolbar;
+//import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MenuPrincipal extends AppCompatActivity {
-
+    Toolbar toolbarMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        toolbarMenu = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbarMenu);
+    }
+
+
+    public boolean onCreateOptionsMenu( Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        switch (menuItem.getItemId()){
+            case R.id.CerrarCession:
+
+                break;
+            case R.id.IrMenu:
+                break;
+        }
+        return true;
     }
 }
