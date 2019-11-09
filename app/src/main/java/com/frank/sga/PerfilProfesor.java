353ebf029910afee39c55e8082bac06c8fe0d5e7
Your Name <you@example.com>
2019-11-09 16:34:03 -0500

    agrego la vista de perfil de profesor

--- app/src/main/java/com/frank/sga/PerfilProfesor.java
+++ app/src/main/java/com/frank/sga/PerfilProfesor.java
@@ -1,18 +1,39 @@
 package com.frank.sga;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
 
 public class PerfilProfesor extends AppCompatActivity {
-    LinearLayout linealPreg1;
+
+    Toolbar toolbarMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_profesor);
-
-        linealPreg1 = findViewById(R.id.porcentagePreg1);
-        linealPreg1.getLayoutParams().width=10;
+        toolbarMenu = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbarMenu);
     }
+
+
+    public boolean onCreateOptionsMenu( Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        switch (menuItem.getItemId()){
+            case R.id.CerrarCession:
+
+                break;
+            case R.id.IrMenu:
+                break;
+        }
+        return true;
+    }
+
 }
